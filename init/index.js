ad052fc5ea64a616a43ce7e9e9049365667209b5
Avishek Amin <avishekamin207@gmail.com>
2026-05-23 10:11:14 +0530

    Added Project Files

---
+++ init/index.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+if (process.env.NODE_ENV != "production") {
+    require("dotenv").config();
+}
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({
+    accessToken: mapToken,
+});
+
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+const mongo_url = "mongodb://127.0.0.1:27017/havenly";
+
+main()
+    .then(() => {
+        console.log("Connected to DB");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+async function main() {
+    await mongoose.connect(mongo_url);
+}
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+
+    for (let obj of initData.data) {
+        let response = await geocodingClient
+            .forwardGeocode({
+                query: obj.location,
+                limit: 1,
+            })
+            .send();
+        obj.owner = "6a07e6cadb9872e2899ddbe4";
+        obj.geometry = response.body.features[0].geometry;
+    }
+    await Listing.insertMany(initData.data);
+    console.log("Data was initialized");
+};
+
+initDB();
